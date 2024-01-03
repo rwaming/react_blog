ece8338f43f52a9831fe23f7d9758bf082676802
RWAMing <art.rwam@gmail.com>
2024-01-03 23:48:28 +0900

    Feat: Add a Component I first try

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ import './App.css';
 function App() {
   // useState(a) = [a, a edit용 함수] a에 여러 자료형 대입 가능
   const [title, titleEdit] = useState(['제목2', '제목1', '제목4', '제목3']);
-  function titleAlign() {
-    const title_copy = [...title]; // deep copy
-    title_copy.sort();
-    titleEdit(title_copy);
-  }
 
   const [good, goodEdit] = useState(0);
   function goodPlus() {
@@ -19,17 +14,16 @@ function App() {
   }
   // plusGood(good + 1); // 무한 렌더링이 생김!! 금지
 
+  // return 내 최상위 태그는 1개만 사용 가능 ex. <div></div><div></div>.. 불가
   return (
     <div className='App'>
       <div className='black-nav'>
         <div>개발 Blog</div>
       </div>
       <div className='list'>
-        <p>
-          <span onClick={goodPlus}>👍</span> {good}
-        </p>
-        <button onClick={titleAlign}>🚻</button>
-        <h3>{title[0]}</h3>
+        <h3>
+          {title[0]} <span onClick={goodPlus}>👍</span> {good}
+        </h3>
         <p>2월 17일 발행</p>
         <hr />
       </div>
@@ -48,6 +42,19 @@ function App() {
         <p>2월 17일 발행</p>
         <hr />
       </div>
+
+      <Modal />
+    </div>
+  );
+}
+
+// component 만들기
+function Modal() {
+  return (
+    <div className='modal'>
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
