f59b32ad63d3e20a2fe67ae9f28d97a97b9e7c98
RWAMing <art.rwam@gmail.com>
2024-01-03 23:28:15 +0900

    Feat: Add State as Study

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+};
--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
-import logo from "./logo.svg";
-import "./App.css";
+/* eslint-disable */
+
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <img src={logo} className="App-logo" alt="logo" />
-        </div>
-    );
+  // useState(a) = [a, a edit용 함수] a에 여러 자료형 대입 가능
+  const [title, titleEdit] = useState(['제목2', '제목1', '제목4', '제목3']);
+  function titleAlign() {
+    const title_copy = [...title]; // deep copy
+    title_copy.sort();
+    titleEdit(title_copy);
+  }
+
+  const [good, goodEdit] = useState(0);
+  function goodPlus() {
+    goodEdit(good + 1);
+  }
+  // plusGood(good + 1); // 무한 렌더링이 생김!! 금지
+
+  return (
+    <div className='App'>
+      <div className='black-nav'>
+        <div>개발 Blog</div>
+      </div>
+      <div className='list'>
+        <p>
+          <span onClick={goodPlus}>👍</span> {good}
+        </p>
+        <button onClick={titleAlign}>🚻</button>
+        <h3>{title[0]}</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className='list'>
+        <h3> {title[1]} </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className='list'>
+        <h3> {title[2]} </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className='list'>
+        <h3> {title[3]} </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+    </div>
+  );
 }
 
 export default App;
