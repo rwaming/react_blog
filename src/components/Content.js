1e3dfe516a76a5272a45dbecdc5e6e0a1790fc07
rwaming <art.rwam@gmail.com>
2024-03-04 02:15:54 +0900

    Docs: Make a txt file, which has description how to show pages as moving

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,22 @@
+import { useEffect } from 'react';
 import './Content.css';
 
 export default function Content() {
+  function toFeed() {
+    // if () {
+    // Profile를 보고 있는 상황 + 아래로 스크롤
+    // => Feed 보여주기
+    // }
+    // else if .. 아니고, Feed가 존재하고 있다면, Feed를 숨긴다.
+  }
+
+  useEffect(() => {
+    window.addEventListener('wheel', toFeed);
+
+    return () => {
+      window.removeEventListener('wheel', toFeed);
+    };
+  }, []);
+
   return <div className='content'>Content</div>;
 }
