2c5290fa72e130f567048f3283aabc3d0fbd29bd
rwaming <art.rwam@gmail.com>
2024-03-03 23:11:55 +0900

    Feat: Ready to make film filter
    I made div.load__bg-film in component Load
    And filterFilm.js which will make basic mood of film filter,
    which is imported to Load.js
    
    Related to: #25 #20

--- src/components/Load.js
+++ src/components/Load.js
@@ -1,5 +1,11 @@
 import './Load.css';
 
+import './functions/filterFilm';
+
 export default function Load() {
-  return <div className='load' />;
+  return (
+    <div className='load'>
+      <div className='load__bg-film' />
+    </div>
+  );
 }
---
+++ src/components/functions/filterFilm.js
@@ -0,0 +1,6 @@
+/*
+  <div className='load__bg-film--3-line-layered' />
+  <div className='load__bg-film--2-line-curvy' />
+  <div className='load__bg-film--1-line' />
+  <div className='load__bg-film--0-particle' />
+*/
